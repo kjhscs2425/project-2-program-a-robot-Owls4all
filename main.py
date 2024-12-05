90fadb2361ff54b4cbfe2cf411778032386aa079
owls4all <owls4all@gmail.com>
2024-12-05 10:40:03 -0800

    potato

--- main.py
+++ main.py
@@ -45,43 +45,7 @@ def findBearings():
     faceInDirection(oldFacing)
     return oldFacing,distanceToRight,distanceToTop
 
-commandOptions=['forward','left','right','back','quit']
-'''#For Testing Purposes:
-closestTooShort = 0
-closestTooFar = 2
-Quit = False
-while not Quit:
-    range = [closestTooShort,closestTooFar]
-    print(range)
-    response = ask('give me a number')
-    duration = float(response)
-    robot.motors(1,-1,duration)
-
-    L,R = robot.sonars()
-    smaller = min(L,R)
-    bigger = max(L,R)
-    
-    if L==R:
-        print('I think we got it.')
-        print('The number we should use is '+str(duration))
-    elif R>L:
-        print('Not Far Enough')
-        print(str(L)+str(R))
-        print('Try again with a bigger number')
-        if duration > closestTooShort:
-            closestTooShort = duration
-    elif L>R:
-        print('Too Far')
-        print(str(L)+str(R))
-        print('Try Again with a smaller number')
-        if duration < closestTooFar:
-            closestTooFar = duration
-    quit=ask('are you done guessing?')
-    if searchList(quit,YesList):
-        break
-    robot.motors(-1,1,duration)
-'''
-    
+commandOptions=['forward','left','right','back','face','where','quit']
 
 
 while Athena == 'the best':
@@ -131,11 +95,6 @@ while Athena == 'the best':
         echo()
     elif command == 'quit':
         break
-    elif command == 'test':
-        faceInDirection(0)
-        print(robotAngle[0])
-        faceInDirection(90)
-        print(robotAngle[0])
     elif command == 'face':
         newHeading = float(ask('in what direction?'))
         faceInDirection(newHeading%360)
